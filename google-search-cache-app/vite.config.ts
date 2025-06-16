d68ba201f05d985abd73d0b515748b12b4fee5a0
amelkumyan <armen.melkumyan@dataart.com>
2025-06-16 12:19:47 +0400

    vite updates

--- google-search-cache-app/vite.config.ts
+++ google-search-cache-app/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+// vite.config.ts
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'happy-dom',
+    setupFiles: [],     // you may add global test setup here
+    globals: true
+  }
 })
